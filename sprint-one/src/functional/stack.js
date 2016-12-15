d22048900d4fd749d05b6e12ca4c698c171274b1
Kevin Jang <pianistkevinjang@gmail.com>
2016-12-15 11:18:51 -0800

    Refactor functional stack to not use Object.keys

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -13,17 +13,15 @@ var Stack = function() {
 
   someInstance.pop = function() {
     // remove last property of storage
-    var keyArray = Object.keys(storage);
-    var deletedValue = storage[keyArray.length-1];
-    delete storage[keyArray.length-1];
-    size--;
-    return deletedValue;
+    if (size > 0) {
+      var deletedValue = storage[size - 1];
+      delete storage[size - 1];
+      size--;
+      return deletedValue;
+    }
   };
 
   someInstance.size = function() {
-    if (size < 0) {
-      return 0;
-    }
     return size;
   };
 
