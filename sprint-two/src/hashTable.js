4501889654cd7c23030e9854095b07c1f11bd1be
Kevin Jang <pianistkevinjang@gmail.com>
2016-12-16 16:08:25 -0800

    Implement hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,3 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -7,14 +5,40 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage.get(index) === undefined) {
+    this._storage.set(index, [[k, v]]);
+  } else {
+    var found = false;
+    this._storage.get(index).forEach(function(pair) {
+      if (pair[0] === k) {
+        pair[1] = v;
+        found = true;
+      }
+    });
+    if (!found) {
+      this._storage.get(index).push([k, v]);
+    }
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var value;
+  this._storage.get(index).forEach(function(pair) {
+    if (pair[0] === k) {
+      value = pair[1];
+    }
+  });
+  return value;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.get(index).forEach(function(pair, i, collection) {
+    if (pair[0] === k) {
+      delete collection[i];
+    }
+  });
 };
 
 
